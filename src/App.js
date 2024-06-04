2296737847ae1bd665c80e7157a6c6608bef2d76
bkhanal87@gmail.com <bkhanal87@gmail.com>
2024-06-04 07:01:00 -0400

    update forms component

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
   <Router>
     <Navigbar />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<About />} />
+      {/* <Route path="/about" element={<About />} /> */}
       <Route path="/projects" element={<Projects />} />
       <Route path="/skills" element={<Skills />} />
       <Route path="/contact" element={<Contact />} />
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,75 @@
-import React from 'react';
+import {React, useState} from 'react';
 import  Form  from 'react-bootstrap/Form';
 import  Button  from 'react-bootstrap/Button';
+import { Container } from 'react-bootstrap';
 
 const Contact = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    }
+
+    const submitForm = (e) => {
+      e.preventDefault()
+
+      console.log({name, email, phone})
+    }
+
     return (
-        <Form>
-            <Form.Group className='mb-3' controlId='formGroupName'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='name' placeholder='Enter Name'/>
+      <>
+        <Container>
+          <Form  onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formGroupName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => {setName(e.target.value)}}
+              />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='formGroupEmail'>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type='email' placeholder='Enter Email'/>
+            <Form.Group className="mb-3" controlId="formGroupEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter Email"
+                value={email}
+                onChange={(e) => {setEmail(e.target.value)}}
+              />
             </Form.Group>
-            <Form.Group className='mb-3' controlId='formGroupMessage'>
-                <Form.Label>Message</Form.Label>
-                <Form.Control as="textarea" placeholder='Type your message here' rows={3} />
-                <Button variant='primary' size='lg' as="input" type="submit" value="Submit" />{' '}
+            <Form.Group className="mb-3" controlId="formGroupphoneNumber">
+              <Form.Label>Phone Number:</Form.Label>
+              <Form.Control
+                type="phone"
+                placeholder="Enter Phone Number"
+                value={phone}
+                onChange={(e) => {setPhone(e.target.value)}}
+              />
             </Form.Group>
-        </Form>
+            <Form.Group className="mb-3" controlId="formGroupMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Type your message here"
+                rows={3}
+              />
+              <br></br>
+              <Button
+                variant="primary"
+                size="lg"
+                as="input"
+                type="submit"
+                value="Submit"
+                onClick={submitForm}
+              />{" "}
+            </Form.Group>
+          </Form>
+        </Container>
+      </>
     );
 }
 
